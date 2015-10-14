b2de6e8032411cb93f46a3c5e7353c854ec49457
U-GAR\sys_lpiasysa <sys_lpiasysa@PGSVLABMISH0009.gar.corp.intel.com>
2015-10-15 00:51:16 +0800

    our first grid working\!

---
+++ App.js
@@ -0,0 +1,43 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    launch: function() {
+
+       console.log('Our First App!');  
+
+
+       var myStore = Ext.create('Rally.data.wsapi.Store', {
+		model: 'User Story',
+		autoLoad: true,
+		listeners: {
+
+			load: function(myStore, myData, success) {
+
+
+				console.log('got data', myStore, myData, success);	
+
+				var myGrid = Ext.create('Rally.ui.grid.Grid', {			
+					store: myStore,
+					columnCfgs: [	
+						'FormattedID', 'Name', 'ScheduleState'
+					]
+				});
+
+
+
+				this.add(myGrid);
+
+				console.log('what is this?', this); 	
+
+			},
+			scope: this
+		},
+		fetch: ['FormattedID', 'Name', 'ScheduleState']
+	});
+	 
+     }
+
+});
+
+
+
